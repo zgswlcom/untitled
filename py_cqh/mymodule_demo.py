e75901931d6a3f24c9f39ea9e517a136bcbb1be1
zgswlaws <858964992@qq.com>
2019-10-11 00:33:59 +0800

    20191010test

---
+++ py_cqh/mymodule_demo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2019/10/11 0:32
+# @Author : cqh
+# @file : mymodule_demo.py.py
+# @Software " PyCharm
+
+import mymodule
+
+print('Version'), mymodule.__version__)
