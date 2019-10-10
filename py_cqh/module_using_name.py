394247873f051e83a785fdd341ebebff52623039
zgswlaws <858964992@qq.com>
2019-10-11 00:21:31 +0800

    20191010test

---
+++ py_cqh/module_using_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2019/10/11 0:20
+# @Author : cqh
+# @file : module_using_name.py
+# @Software " PyCharm
+if __name__ == '__main__':
+    print('This program is being run by itself')
+else:
+    print('I am being imported from another module')
