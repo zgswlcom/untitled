5fbecff3f3524b4943cab955545a1fe5a9a9309a
zgswlaws <858964992@qq.com>
2019-10-11 00:29:03 +0800

    20191010test

---
+++ py_cqh/mymodule.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2019/10/11 0:27
+# @Author : cqh
+# @file : mymodule.py
+# @Software " PyCharm
+
+def say_hi():
+    print('Hi,this is mymodule speaking.')
+
+__version__ = '0.1'
